44d234fc153c011ac529a0ba381555de54fdcadb
kinbech2018 <kushaladhikari144@gmail.com>
2022-04-27 08:19:21 +0545

    first

---
+++ app/Http/Livewire/Admin/Orders.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+
+class Orders extends Component
+{
+    public function render()
+    {
+
+        return view('livewire.admin.orders')->layout('layouts.base');
+    }
+}
